94a740326ae6e44da5351fc6d02c1e35637c79f9
Nathaniel Fruni <fruninathan@gmail.com>
2024-06-13 11:36:28 +0200

    Update AdminRegistrationView.vue

--- src/views/admin/AdminRegistrationView.vue
+++ src/views/admin/AdminRegistrationView.vue
@@ -8,18 +8,19 @@ const last_name = ref("");
 const email = ref("");
 const password = ref("");
 const c_password = ref("");
+const successMessage = ref("");
 
 const registerAdmin = async () => {
   try {
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)access_token\s*=\s*([^;]*).*$)|^.*$/, "$1");
 
-    const response = await axios.post(import.meta.env.VITE_API_ENDPOINT + '/api/auth/register',  {
-      first_name: first_name.value,
-      last_name: last_name.value,
-      email: email.value,
-      password: password.value,
-      c_password: c_password.value
-    },
+    const response = await axios.post(import.meta.env.VITE_API_ENDPOINT + '/api/auth/register', {
+        first_name: first_name.value,
+        last_name: last_name.value,
+        email: email.value,
+        password: password.value,
+        c_password: c_password.value
+      },
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -27,11 +28,21 @@ const registerAdmin = async () => {
       });
 
     console.log(response.data);
+    
+    first_name.value = "";
+    last_name.value = "";
+    email.value = "";
+    password.value = "";
+    c_password.value = "";
+
+    successMessage.value = "Nový admin úspešne pridaný.";
   } catch (error) {
     console.error('An error occurred:', error);
+    successMessage.value = "Nastala chyba pri registrácii. Skúste to znova.";
   }
 };
 </script>
+
 <template>
   <section>
     <div class="container text-center d-flex justify-content-center align-content-center vh-100">
@@ -109,6 +120,9 @@ const registerAdmin = async () => {
                         >
                       </div>
                     </div>
+                  </div>
+                  <div v-if="successMessage" class="alert alert-dark mt-3 text-white" role="alert">
+                    {{ successMessage }}
                   </div>
                 </form>
               </div>
