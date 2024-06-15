e89f3fea1ef6c052ba3fe2ee4843567c0014af01
Nathaniel Fruni <fruninathan@gmail.com>
2024-06-15 14:19:44 +0200

    dashboard start

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -1,9 +1,16 @@
 <script setup>
-
+import AttendeesTable from '@/views/admin/components/AttendeesTable.vue'
 </script>
 
 <template>
-<h1>Hi</h1>
+
+  <div>
+    <div class="col-8 m-3">
+      <h3 class="text-center">Zoznam účatníkov</h3>
+      <AttendeesTable />
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
